48fc6b1922843460200d6e601d66af2ec9622644
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-05-23 16:28:33 -0300

    Cria componente CardCase

---
+++ src/components/CardCase/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ButtonAccess } from '../Ui';
+import * as P from 'prop-types';
+import './styles.scss';
+
+export const CardCase = ({ cases }) => {
+  return (
+    <div className="cards-container__card">
+      <span className="cards-container__card__case-title">{cases.title}</span>
+      <p className="cards-container__card__description">{cases.description}</p>
+      <ButtonAccess />
+    </div>
+  );
+};
+
+CardCase.propTypes = {
+  cases: P.object,
+};
