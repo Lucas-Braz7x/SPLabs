868fd96149dbf86bb36dacb846641727024de74e
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-05-22 23:49:32 -0300

    Cria componente Slogan

---
+++ src/components/Slogan/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles.scss';
+
+export const Slogan = () => {
+  return (
+    <>
+      <div className="cases-section__slogan slogan">
+        <h2 className="slogan__title">
+          <span className="slogan__title--display-block">problemas complexos. </span>
+          Soluções <span className="slogan__title--text-style">complexas</span>
+          <span className="slogan__title--text-italic"> criativas_</span>
+        </h2>
+      </div>
+      <div className="cases-section__sub-title">
+        <h3 className="cases-section__sub-title__text-content">
+          confira nossos cases de sucesso que vão além do mundo juridico.
+        </h3>
+      </div>
+    </>
+  );
+};
