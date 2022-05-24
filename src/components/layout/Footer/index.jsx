6ad2b66d15db4efa5293c936d373443c171ea4af
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-05-23 22:27:02 -0300

    Cria componente Footer

---
+++ src/components/layout/Footer/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { AnchorLink } from '../../Ui';
+import './styles.scss';
+
+export const Footer = () => {
+  return (
+    <footer className="container--responsive footer">
+      <div className="footer__information">
+        <span>SP labs</span>
+
+        <div className="footer__links links">
+          <div className="links__items">
+            <span className="links__items__text">SP Labs</span>
+            <AnchorLink to="#" elementClassName="links__items__link">
+              Política de Privacidade
+            </AnchorLink>
+          </div>
+          <div className="links__items">
+            <span className="links__items__text">sem processo</span>
+            <ul className="links__items__list">
+              <li>
+                <AnchorLink to="#" elementClassName="links__items__link">
+                  site
+                </AnchorLink>
+              </li>
+              <li>
+                <AnchorLink to="#" elementClassName="links__items__link">
+                  produtos
+                </AnchorLink>
+              </li>
+              <li>
+                <AnchorLink to="#" elementClassName="links__items__link">
+                  blog
+                </AnchorLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="border">{/* border */}</div>
+
+      <span className="footer__author">SP Labs 2022. Feito por Lucas Braz Dutra </span>
+    </footer>
+  );
+};
